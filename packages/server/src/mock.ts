f91ecb3b9b7a2117c158042aab7f4014ed8b01a2
hojoon <ghwns107@naver.com>
2023-03-26 03:05:37 +0900

    mock 데이터 추가중

--- packages/server/src/mock.ts
+++ packages/server/src/mock.ts
@@ -3,12 +3,6 @@ export default [
     username: "Abby",
     thumbnailImageUrl: "/1.png",
   },
-  {
-    username: "Abraham",
-    thumbnailImageUrl: "/2.png",
-  },
-  {
-    username: "Nadia",
-    thumbnailImageUrl: "/3.png",
-  },
+  { username: "Abraham", thumbnailImageUrl: "/2.png" },
+  { username: "Nadia", thumbnailImageUrl: "/3.png" },
 ];
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -29,7 +29,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-sequelize.sync({ force: true });
+sequelize.sync({ force: false });
 
 app.use("/", routes);
 
